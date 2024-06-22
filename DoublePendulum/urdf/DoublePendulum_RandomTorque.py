32fb6c5e93864d852bf8672f9afa885fa178b974
fluci0 <134448340+fluci0@users.noreply.github.com>
2024-06-22 13:56:32 -0600

    DP Data - Upload

--- DoublePendulum/urdf/DoublePendulum_RandomTorque.py
+++ DoublePendulum/urdf/DoublePendulum_RandomTorque.py
@@ -26,9 +26,9 @@ planeId = p.loadURDF("plane.urdf")
 #Loading model
 cubeStartPos1 = [0, 0, 0]
 cubeStartOrientation1 = p.getQuaternionFromEuler([0, 0, 0])
-Id1 = p.loadURDF("DoublePendulum.urdf", cubeStartPos1, cubeStartOrientation1, useFixedBase = False)
+Id1 = p.loadURDF("DoublePendulum.urdf", cubeStartPos1, cubeStartOrientation1, useFixedBase = True)
 
-camera = p.resetDebugVisualizerCamera(1, 90, -15, [0, 0, 0])
+camera = p.resetDebugVisualizerCamera(1, 0, -45, [0, 0, 0])
 
 #Appereance
 p.changeVisualShape(Id1, linkIndex = -1, rgbaColor = (1, 0, 1, 1)) #Base
@@ -51,7 +51,7 @@ _ = p.enableJointForceTorqueSensor(Id1, 1)
 
 #Simulation
 start = 0.0 
-end = 1.0 
+end = 300.0 
 
 steps = int((end - start) / TimeStep) #Número de pasos
 t = [0] * steps #Inicializando lista de pasos de tiempo
@@ -61,63 +61,77 @@ for s in range(steps):
     
 marker = p.createVisualShape(p.GEOM_SPHERE, radius = 0.005, rgbaColor = (0.905, 0.443, 0.956, 1))
 
-#States
-pos1 = []
-vel1 = []
-avel1 = []
+#Vector: to store States
+j1_pos = []
+j2_pos = []
+l1_angpos = []
+l2_angpos = []
+
 tor1 = []
 
-pos1_theta = []
-vel1_theta = []
-pos2_theta = []
-vel2_theta = []
 
 for r in tqdm(range(len(t))):
-    #Reading states
-    l1_states = p.getLinkState(Id1, 1, computeLinkVelocity = 1)
-    l1_pos = l1_states[0]
-    l1_ori = l1_states[1]
-    l1_vel = l1_states[6]
-    l1_avel = l1_states[7]
-    
-    l1_euler = p.getEulerFromQuaternion(l1_ori)
-    l1_oriy = l1_euler[1] #rad
-    l1_oriy_p = l1_states[7][1] #rad/s
-    
-    print('l1_pos', l1_pos)
-    print(l1_oriy)
-    print(l1_oriy_p)
-    
     #Random Torque
-    uop1 = random.uniform(-0.1, 0.1)
+    uop1 = random.uniform(-3, 3)
     p.setJointMotorControl2(Id1, 0, controlMode = p.TORQUE_CONTROL, force = uop1)
     
-    pos1.append(l1_pos[0])
-    vel1.append(l1_vel[0])
-    avel1.append(l1_avel)
+    #Joint States
+    j1_states = p.getJointState(Id1, 0)
+    j1_p = j1_states[0]
+    j1_v = j1_states[1]
+    
+    j2_states = p.getJointState(Id1, 1)
+    j2_p = j2_states[0]
+    j2_v = j2_states[1]
+    
+    #Link states
+    L1_states = p.getLinkState(Id1, 0, computeLinkVelocity = 1)
+    l1_ori = L1_states[1]
+    l1_eori = p.getEulerFromQuaternion(l1_ori)
+    j1_theta = l1_eori[1]
+    
+    L2_states = p.getLinkState(Id1, 1, computeLinkVelocity = 1)
+    l2_ori = L2_states[1]
+    l2_eori = p.getEulerFromQuaternion(l2_ori)
+    j2_theta = l2_eori[1]
+    
+    #print('l1_eori', l1_eori) #For this case we are interested in rotation angle in Y axis
+    
+    j1_pos.append(j1_p)
+    j2_pos.append(j2_p)
+    l1_angpos.append(j1_theta)
+    l2_angpos.append(j2_theta)
     tor1.append(uop1)
-    pos1_theta.append(l1_oriy)
-    vel1_theta.append(l1_oriy_p) 
+    
+    #print('j1_p', j1_p)
+    #print('j1_v', j1_v) 
 
     p.stepSimulation()
     time.sleep(0.1)
 
+print('CSV creation')
 
-np.savetxt('Modulo_data_1000.csv', [p for p in zip(t, pos1_theta, vel1_theta, tor1)], delimiter = ',', fmt = '%s') 
+np.savetxt('Pendulum_data_1000.csv', [p for p in zip(t, j1_pos, j2_pos, l1_angpos, l2_angpos, tor1)], delimiter = ',', fmt = '%s') 
+
 
 #Plotting
 font = {'family': 'sans-serif', 'color': 'aqua',
             'weight': 'bold', 'size': 12}       
 
-fig, (ax1, ax2) = plt.subplots(1, 2)
-ax1.set_title("Angular Position \n VS \n Time", fontdict = font)
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+ax1.set_title("[L1] Angular Position \n VS \n Time", fontdict = font)
 ax1.set_xlabel('Time', fontdict = font)
 ax1.set_ylabel('Angular Position', fontdict = font)
-ax2.set_title("Random Torque \n VS \n Time", fontdict = font)
+ax2.set_title("[L2] Angular Position \n VS \n Time", fontdict = font)
 ax2.set_xlabel('Time', fontdict = font)
-ax2.set_ylabel('Torque', fontdict = font)
-ax1.plot(t, pos1_theta, 'Dw', label = 'Angle') 
-ax2.plot(t, tor1, 'Dw', label = 'Random Torque')  
+ax2.set_ylabel('Angular Position', fontdict = font)
+ax3.set_title("Random Torque \n VS \n Time", fontdict = font)
+ax3.set_xlabel('Time', fontdict = font)
+ax3.set_ylabel('Torque', fontdict = font)
+
+ax1.plot(t, l1_angpos, 'Dw', label = 'Angle') 
+ax2.plot(t, l2_angpos, 'Dw', label = 'Angle') 
+ax3.plot(t, tor1, 'Dw', label = 'Random Torque')  
  
 
 
